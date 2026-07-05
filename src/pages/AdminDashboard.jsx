bdcae30abe24d8c69e811ef32532ab55aebd8f0d
Ahmedino <ahmedino3313@gmail.com>
2026-07-05 15:12:52 +0100

    update states

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -23,7 +23,7 @@ const US_STATES = [
     'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
     'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
     'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
-    'West Virginia', 'Wisconsin', 'Wyoming',
+    'West Virginia', 'Wisconsin', 'Wyoming', 'Ontario',
 ];
 
 const STATE_CODES = {
